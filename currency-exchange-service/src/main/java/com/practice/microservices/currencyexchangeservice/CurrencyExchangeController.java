75c53e840a00ac1855ec4fe16dc520b3011d944e
Larry <larryosak@gmail.com>
2021-07-24 16:04:34 -0700

    add parameters, getters, setters to CurrencyExchange class

--- currency-exchange-service/src/main/java/com/practice/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/practice/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,15 +1,19 @@
 package com.practice.microservices.currencyexchangeservice;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
 
 @RestController
 public class CurrencyExchangeController {
 
-    @GetMapping("/currency-exchange/from/USD/to/INR")
-    public CurrencyExchange retrieveExchangeValue() {
-        CurrencyExchange currencyExchange = new CurrencyExchange();
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(@PathVariable String from,
+                                                  @PathVariable String to) {
 
+        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(100));
         return currencyExchange;
     }
 
